429eb1f6a6824d1685f561b75259999668f621e9
bjasolya <139614030+bjasolya@users.noreply.github.com>
2025-02-07 17:53:13 +0000

    type of data and conversion

--- basics/datatype.js
+++ basics/datatype.js
@@ -1,6 +1,19 @@
 let age=18
 let name="bhavna"
 let isloggedin=true
+let state=null
+
 console.log(typeof age)
 console.log(typeof name)
 console.log(typeof isloggedin)
+console.log(typeof state)
+
+ /*data types
+1.number
+2.string
+3.boolean
+4.symbol (for component uniqueness)
+5.object
+6.undefined
+7.null
+*/
---
+++ basics/datatype_conversion.js
@@ -0,0 +1,18 @@
+let age="213a"
+//console.log(typeof age)
+let intonumber=Number(age)
+//console.log(typeof intonumber)
+//console.log(intonumber)
+//null=>conversion into number will be 0
+//undefined=>conversion into number will be NaN
+let isloggedin=1
+let booleanlogged=Boolean(isloggedin)
+//console.log(typeof booleanlogged)
+//console.log(booleanlogged)
+//1=>true||0=>false
+//" "=>false
+//"bhavna"=>true
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
